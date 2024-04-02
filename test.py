a80a563e60df29886513e46e68ccd029dc2500a0
Lootchy <zcolucci@gaming.tech>
2024-04-02 14:57:58 +0200

    Background and border Update

--- test.py
+++ test.py
@@ -2,32 +2,48 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
-image = pygame.image.load("tile_0000.png").convert()
+
+screen = pygame.display.set_mode((1792, 1024))
+
+
+image = pygame.image.load("tile_0000.png").convert_alpha() 
+background = pygame.image.load("fond.webp").convert()
+image = pygame.transform.scale(image, (96, 96))
+rect = image.get_rect()
+
 clock = pygame.time.Clock()
 continuer = True
 x = 0
 y = 0
+moveSpeed = 6
 
 while continuer:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             continuer = False
+
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_LEFT]:
-        x -= 1
-    if pressed[pygame.K_RIGHT]:
-        x += 1
-    if pressed[pygame.K_DOWN]:
-        y += 1  
-    if pressed[pygame.K_UP]:
-        y -= 1
+    if pressed[pygame.K_LEFT] and rect.left > 0:
+        rect.x -= moveSpeed
+    if pressed[pygame.K_RIGHT] and rect.right < 1792:
+        rect.x += moveSpeed
+    if pressed[pygame.K_DOWN] and rect.bottom < 1024:
+        rect.y += moveSpeed
+    if pressed[pygame.K_UP] and rect.top > 0:
+        rect.y -= moveSpeed
     if pressed[pygame.K_SPACE]:
         continuer = False
 
-    screen.fill((0, 0, 0))
-    screen.blit(image, (x, y))  
+   
+    screen.blit(background, (0, 0))
+    
+
+    screen.blit(image, (rect))
+    
+
     pygame.display.flip()
+    
+   
     clock.tick(60)
 
 pygame.quit()
