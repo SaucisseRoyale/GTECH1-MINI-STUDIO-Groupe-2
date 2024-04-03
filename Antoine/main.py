efe910692910f8fc27cc55cc149a2b7d3d28c945
DEROIDE Antoine <147041467+SaucisseRoyale@users.noreply.github.com>
2024-04-03 15:35:05 +0200

    giveup.py

--- Antoine/main.py
+++ Antoine/main.py
@@ -7,13 +7,51 @@ clock = pygame.time.Clock()
 # Creation de la fenetre de jeu
 x_screen : int = 1080
 y_screen : int = 720
+
+tile_size = 50
+
 pygame.display.set_caption("Python_Game")
 screen = pygame.display.set_mode((x_screen,y_screen))
+
+
+
+class World:
+    def __init__(self, data):
+        self.tile_list = []
+        dirt_img = pygame.image.load("Antoine/tile_0001.png").convert_alpha()
+        grass_img = pygame.image.load("Antoine/tile_0002.png").convert_alpha()
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    self.tile_list.append((img, img_rect))
+                elif tile == 2:
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    self.tile_list.append((img, img_rect))
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+
 
 class Player :
     # Variables JOUEUR
     def __init__(self, x, y) -> None:
-        self.rect = pygame.Rect(520,340,20,20)
+        img = pygame.image.load("Antoine/sanic.gif")
+        self.icon = pygame.transform.scale(img, (56,56))
+        self.rect = self.icon.get_rect()
         self.jump_force : int = 5
         self.jump_count : int = 1
         self.velocity : float = 0
@@ -38,9 +76,6 @@ class Player :
             self.jump_count -= 1 # On enleve un saut du compteur (=/= 0 en vue du double saut)
             moveAlongY -= 2
         
-        # Condition qui, dans le cadre d'une collision avec le sol, 
-        #self.jump_count += 1
-        
         # Appliquer la gravité
         self.velocity += 1 * 0.016 
         if self.velocity > 10:  
@@ -48,33 +83,46 @@ class Player :
 
         moveAlongY += self.velocity
 
-        # Vérifier le contact avec le sol
-        if self.rect.bottom + dy > x_screen:
-            dy = y_screen - self.rect.bottom
+        # Vérifier le contact avec les bords de l'écran
+        if self.rect.bottom + moveAlongY > x_screen:
+            moveAlongY = y_screen - self.rect.bottom
             self.velocity = 0 
-            self.jumped = False
+            self.jump_count += 1
+
+        # Vérifier les collisions avec le sol
+        for tile in world.tile_list:
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
+                elif self.vel_y >= 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
+                    self.on_ground = True
 
         # Mise à jour des coordonnées du joueur
         self.rect.x += moveAlongX
         self.rect.y += moveAlongY
-
-        pygame.draw.rect(screen, (255,0,0), self.rect)
-        pygame.display.flip()
         
-        screen.blit(screen, self.rect)
+        # Affichage
+        screen.blit(self.icon, self.rect)
 
 
-
-
-                
 
     
 
 # MAIN
 def main() :
     # Preparation et lancement de la musique
-    pygame.mixer.music.load(filename="music.mp3")
+    pygame.mixer.music.load(filename="Antoine/music.mp3")
     pygame.mixer.music.play(-1)
+
+    # Préparation du fond du niveau
+    background = pygame.image.load("Antoine/background.png").convert()
+    background = pygame.transform.scale(background, (x_screen, y_screen))
 
     isRunning : bool = True
 
@@ -83,30 +131,51 @@ def main() :
     gravity : int = 1
 
 
+    world_data = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 1, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
 
+    world = World(world_data)
+
+    rect_player = Player(520, 340)
     
 
     # BOUCLE DE JEU
     while isRunning :
 
+        # Initialisation du jeu / décor
         clock.tick(60)
+        screen.blit(background, (0,0))
         
-        
-        
+
         for event in pygame.event.get() : 
+            if event.type == pygame.QUIT :
+                isRunning = False
+
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 isRunning = False
 
-
-
+        # Actualisation de l'affichage
+        world.draw()
+        rect_player.update()
+        pygame.display.flip()
+        
+        
             
-    
-    rect_player = Player(520, 340)
-
-
-    # Actualisation de l'affichage
-    screen.fill(0)
-    
     
 
     
@@ -116,5 +185,3 @@ def main() :
     
 
 main()
-    
-                
