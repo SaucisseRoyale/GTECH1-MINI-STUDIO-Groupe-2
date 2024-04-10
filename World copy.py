c4460614e6b259c78c50820f913057d1f92a1779
Lootchy <zcolucci@gaming.tech>
2024-04-10 13:42:31 +0200

    world end

--- World copy.py
+++ World copy.py
@@ -187,29 +187,35 @@ class Player:
         dy += self.vel_y
 
         # Check collisions
-        # Check collisions
-        for tile in world.tile_list:
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                dx = 0
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                if tile[2] == 21:  
-                    self.vel_y = -self.jump_power * 1.5
-                    self.on_ground = False
-                    break 
-
-                # Gestion des autres collisions
-                if self.vel_y < 0:
-                    dy = tile[1].bottom - self.rect.top
-                    self.vel_y = 0
-                elif self.vel_y >= 0:
-                    dy = tile[1].top - self.rect.bottom
-                    self.vel_y = 0
-                    self.on_ground = True
-
-
-        # Update player coordinates
         self.rect.x += dx
         self.rect.y += dy
+
+        # Nouvelle gestion des collisions avec les tuiles en utilisant les masques
+        for tile in world.tile_list:
+            # Obtention de la position relative du joueur par rapport à la tuile pour la vérification des masques
+            offset_x = tile[1].x - self.rect.x
+            offset_y = tile[1].y - self.rect.y
+            
+            # Vérification de la collision entre le masque du joueur et celui de la tuile
+            collision_point = self.mask.overlap(tile[3], (offset_x, offset_y))
+            
+            if collision_point:
+                # Collision détectée, gérer en fonction de la position et du type de collision
+                if dy > 0:  
+                    self.rect.y = tile[1].y - self.height
+                    dy = 0
+                    self.on_ground = True
+                    self.vel_y = 0
+                elif dy < 0:
+                    self.rect.y = tile[1].bottom
+                    dy = 0
+                    self.vel_y = 0
+                if dx > 0:  
+                    self.rect.x = tile[1].left - self.width
+                    dx = 0
+                elif dx < 0:
+                    self.rect.x = tile[1].right
+                    dx = 0
 
 
 
